db5df5dc67f957198a8d573092365c6eb17e7c4c
Jason Guo <137559636+JasonGuo15@users.noreply.github.com>
2026-08-25 12:21:40 -0400

    Add files via upload

---
+++ Movie.h
@@ -0,0 +1,19 @@
+
+
+
+class Movie
+{
+private:
+	string title;
+	string genre;
+	int year;
+	double rating;
+
+public:
+	Movie();
+	Movie(string t, string g, int y, double r);
+	string getTitle() const;
+	string getGenre() const;
+	int getYear() const;
+	double getRating();
+	void display() const;
